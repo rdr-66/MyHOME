19651f34dac12d5485c1bcaa991ed59b13f2c0b1
rdr-66 <raffaele@dericcardis.it>
2024-10-09 17:45:39 +0200

    Update __init__.py
    add alarm

--- custom_components/myhome/__init__.py
+++ custom_components/myhome/__init__.py
@@ -27,7 +27,7 @@ from .const import (
 from .validate import config_schema, format_mac
 from .gateway import MyHOMEGatewayHandler
 
-PLATFORMS = ["light", "switch", "cover", "climate", "binary_sensor", "sensor"]
+PLATFORMS = ["light", "switch", "cover", "climate", "binary_sensor", "sensor", "alarm"]
 
 
 async def async_setup(hass, config):
